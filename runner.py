19cddf95d3a2db1d232d9d875da01062407c4ea6
Vladimir Pichugin <vladimir@pichug.in>
2023-01-17 18:15:50 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                         format=u'>> [%(asctime)s] %(message)s',
                         datefmt='%y.%m.%d %H:%M:%S')
 
-    cmd = "python3.6 -u run.py"
+    cmd = "python -u run.py"
     p = None
 
     try:
